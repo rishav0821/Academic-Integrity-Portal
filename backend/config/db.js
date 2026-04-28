d9e6e603a99c867c091c6b8efc7b2c242f6df262
rishav0821 <rishav.18271@stu.upes.ac.in>
2026-04-28 17:30:21 +0530

    yes

--- backend/config/db.js
+++ backend/config/db.js
@@ -5,7 +5,7 @@ dotenv.config();
 
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect("mongodb+srv://rishavkr696:Rishav007@cluster0.ae9rzqg.mongodb.net/?appName=Cluster0");
+    const conn = await mongoose.connect(process.env.MONGO_URI);
     console.log(`MongoDB Connected: ${conn.connection.host}`);
   } catch (error) {
     console.error(`Error: ${error.message}`);
